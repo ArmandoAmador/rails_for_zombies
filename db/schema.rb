57d2cda9e5965096bc7907dc81c33bcb178605fe
Armando Amador <arm.amador@gmail.com>
2015-03-03 21:14:22 -0500

    Remove age from zombies table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150304015949) do
+ActiveRecord::Schema.define(version: 20150304021244) do
 
   create_table "zombies", force: :cascade do |t|
     t.string   "name"
     t.text     "bio"
-    t.integer  "age"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.string   "email"

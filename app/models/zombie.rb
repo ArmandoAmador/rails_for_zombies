6b3bb60e46636d34f8a8913ab41471569b2efd47
Armando Amador <arm.amador@gmail.com>
2015-03-05 10:37:00 -0500

    add role and assignment tables  - Add has_many, through, belongs_to relationship between assignment, role and zombies  - add migrations

--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -1,6 +1,8 @@
 # class zombie
 class Zombie < ActiveRecord::Base
   has_one :brain, dependent: :destroy
+  has_many :assignments
+  has_many :roles, through: :assignments
   before_save :make_rotting
   scope :rotting, -> { where(rotting: true) }
   scope :fresh, -> { where('age < 20') }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150305113913) do
+ActiveRecord::Schema.define(version: 20150305151259) do
+
+  create_table "assignments", force: :cascade do |t|
+    t.integer "zombie_id"
+    t.integer "role_id"
+  end
+
+  add_index "assignments", ["role_id"], name: "index_assignments_on_role_id"
+  add_index "assignments", ["zombie_id"], name: "index_assignments_on_zombie_id"
 
   create_table "brains", force: :cascade do |t|
     t.integer  "zombie_id"
@@ -19,6 +27,10 @@ ActiveRecord::Schema.define(version: 20150305113913) do
     t.string   "flavor"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "title"
   end
 
   create_table "zombies", force: :cascade do |t|

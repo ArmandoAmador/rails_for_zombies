5a4475646312a511a77ec085759a218715ce43b4
Armando Amador <arm.amador@gmail.com>
2015-03-10 00:21:54 -0400

    Add AJAXified create zombie forms
     - Add jquery-ui-rails
     - Remove make_rotting method from zombies model
     - Refactor zombie view to work better with ajax form

--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -3,12 +3,9 @@ class Zombie < ActiveRecord::Base
   has_one :brain, dependent: :destroy
   has_many :assignments
   has_many :roles, through: :assignments
-  before_save :make_rotting
   scope :rotting, -> { where(rotting: true) }
   scope :fresh, -> { where('age < 20') }
   scope :recent, -> { order('created_at desc').limit(3) }
 
-  def make_rotting
-    self.rotting = true if age > 20
-  end
+  validates :name, presence: true
 end
